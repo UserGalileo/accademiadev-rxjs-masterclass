b6ba57ab065657f16d20ce3d2d9e47d53ef4a65a
Michele Stieven <usergalileo@gmail.com>
2020-04-07 15:08:49 +0200

    First commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  verbose: true,
+  rootDir: './',
+  transform: {
+    '^.+\\.js?$': 'babel-jest',
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+
+const config = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html')
+    }),
+    new CopyPlugin([{ from: 'assets', to: 'assets' }]),
+    new WriteFilePlugin()
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  stats: 'minimal',
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    open: false,
+    contentBase: './dist',
+    inline: true,
+    port: 4000,
+  },
+};
+
+module.exports = config;
